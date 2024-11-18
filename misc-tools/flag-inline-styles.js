4bf522a16641a99d055361f51eaffe0b47237a2b
Brown Juice Co <5776635+brimwd@users.noreply.github.com>
2024-11-19 08:44:47 +0900

    add imags

---
+++ misc-tools/flag-inline-styles.js
@@ -0,0 +1,5 @@
+(function flagInline () {
+	$('head').append('<style type="text/css" for="flag-broken-links">[style]{border:red 3px solid!important;}</style>');
+	console.log('inline styles flagged');
+})();
+
---
+++ misc-tools/generate-qr.js
@@ -0,0 +1,4 @@
+function generateQR () {
+	ajax:
+	https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=URL
+}
