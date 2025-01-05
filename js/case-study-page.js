814e8ba3f0bd6ddd427cbfc40b1b977a69b2f498
Brown Juice Co <5776635+brimwd@users.noreply.github.com>
2025-01-05 14:36:00 +0900

    consolidate reused head tags

--- js/case-study-page.js
+++ js/case-study-page.js
@@ -2,14 +2,14 @@ function configureSinglePage() {
 	$(`head`)
 		.prepend($(`<script async src="https://www.googletagmanager.com/gtag/js?id=G-DL67DCGTZ7"></script><script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-DL67DCGTZ7');</script>`))
 		.prepend($(`<link rel="stylesheet" type="text/css" href="style.css">`))
-		.prepend($(`<link href="https://fonts.googleapis.com/css?family=Raleway:300,600,700&display=swap" rel="stylesheet">`))
+		.prepend($(`<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Bitter:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">`))
 		.prepend($(`<script src="https://kit.fontawesome.com/fe72761270.js" crossorigin="anonymous"></script>`))
 		.prepend($(`<meta charset="UTF-8">`))
 		.prepend($(`<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">`))
 		.prepend($(`<meta name="author" content='B Donahue ("Leo")'>`))
 		.prepend($(`<meta name="description" content="UX, web design, & front-end development portfolio for Leo Donahue, UX Design Manager based in Tokyo, Japan">`))
 		.prepend($(`<meta name="keywords" content="product,product design,manager,UX,UX design,UX designer,user experience,design,designer,UI,HTML,CSS,JavaScript,Illustrator,Adobe,Photoshop,freelance,contractor,developer,front-end,front end,portfolio,graphic design,graphics,web,web development,for hire,resume,jQuery,storyboarding,prototyping,research,user research,researcher,information architecture,taxonomy">`))
-		.prepend($(`<meta name="format-detection" content="telephone=no">;`));
+		.prepend($(`<meta name="format-detection" content="telephone=no">`));
 
 	$(`.case-study`)
 		.prepend($(`<nav class="mb-5"><a href="index.html"><img src="favicon.ico" />Portfolio</a></nav>`))
